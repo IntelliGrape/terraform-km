1f46329c42a82ac74cc8a75170f3e93a0c2af637
theakshayraj <theakshayraj@outlook.com>
2021-06-19 16:42:06 +0530

    Working on Modules

--- rahuljkm/Akshay/main.tf
+++ rahuljkm/Akshay/main.tf
@@ -1,8 +1,4 @@
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "terraform-Akshay-km"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+module "ecr" {
+source = "../modules/ecr"
+reponame = "terraform-km-akshay"
 }

320cd2631efbba2648ed1d33047ac949ec2076f8
nitinpainoli <nitin.nn711@gmail.com>
2021-06-19 14:27:56 +0530

    module

--- Harkesh/Nitin/main.tf
+++ Harkesh/Nitin/main.tf
@@ -1,9 +1,7 @@
-resource "aws_ecr_repository" "harkesh_nitin" {
-  name                 = var.name
-  image_tag_mutability = var.image_tag_mutability
+module "ecr" {
+source = "./modules/ecr/"
 
-  image_scanning_configuration {
-    scan_on_push = var.scan_on_push
-  }
+
+
 }
 

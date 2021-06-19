f594c892a891bbfee953cf75ffa453dc19b430c2
Nihit Jain <nihit.jain@tothenew.com>
2021-06-19 14:45:49 +0530

    modules

--- Harkesh/Nihit/main.tf
+++ Harkesh/Nihit/main.tf
@@ -1,8 +1,4 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "terraform-km-nihit"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+module "ecr" {
+  source = "./modules"
+  ecr_name = var.name
 }

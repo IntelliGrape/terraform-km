bab00b9f3fddac55e53f872289255ddee91181a5
Rahul Jaiswal <rahul.jaiswal@tothenew.com>
2021-06-19 14:48:44 +0530

    fixed variable

--- rahuljkm/examples/main.tf
+++ rahuljkm/examples/main.tf
@@ -1,4 +1,4 @@
 module "ecr" {
 source = "../rahulj/modules/ecr"
-reponame = "terraform-km-rahul"
+reponame = var.repo_name
 }
---
+++ rahuljkm/examples/variables.tf
@@ -0,0 +1,6 @@
+variable "repo_name" {
+  description = "ecr repo name"
+  type = string
+  default = "terraform-km-rahul"
+}
+}

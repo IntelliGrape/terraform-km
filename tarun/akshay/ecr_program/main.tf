48365c709cb8c809c497b196f6033837f071c9c9
Akshay Shrivastava <akshayshrivastava@Akshays-MacBook-Pro.local>
2021-06-19 14:07:00 +0530

    added modules

--- tarun/akshay/ecr_program/main.tf
+++ tarun/akshay/ecr_program/main.tf
@@ -9,12 +9,9 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_ecr_repository" "foo" {
-  name                 = "tarunsquad_akshay_repo"
-  image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
+module "ecr_program" {
+  source = "../ecr_module"
+  ecr_name = var.ecr_name
+  
+} 

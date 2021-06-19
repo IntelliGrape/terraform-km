86b3bcd0441271766a0f91ed5c66e272ab083c65
khandelwaly <yash.khandelwal.inbox@gmail.com>
2021-06-19 11:55:56 +0530

    deleted dupliate files

--- spartans/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "ecr1" {
-  name                 = "km-terraform-session-1"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}

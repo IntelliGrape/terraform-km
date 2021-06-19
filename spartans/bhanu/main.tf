accf7e07cffea8571159e0d379d098e29c8eb967
Bhanu Bhadauria <bhanu.bhadauria@tabcorp.com.au>
2021-06-19 12:43:34 +0530

    ecr repo applied code

--- spartans/bhanu/main.tf
+++ spartans/bhanu/main.tf
@@ -1,6 +1,7 @@
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "terraform-spartans-squad-km"
-  image_tag_mutability = "MUTABLE"
+
+resource "aws_ecr_repository" "ecr" {
+  name                 = "terraform-km-bhanu"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true

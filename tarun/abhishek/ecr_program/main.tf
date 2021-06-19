c691e7335357b3f946a701d177e526244fd74daa
abhishek chauhan <abhishek.chauhan1@tothenew.com>
2021-06-19 14:08:33 +0530

    created module

--- tarun/abhishek/ecr_program/main.tf
+++ tarun/abhishek/ecr_program/main.tf
@@ -1,8 +1,8 @@
 resource "aws_ecr_repository" "demo" {
-  name                 = "km-demo-abhishek"
-  image_tag_mutability = "MUTABLE"
+  name                 = var.name
+  image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = var.imagescan
   }
 }

655f4a8eab66ded7c4b7c89cdee617e6427547bc
Akash <akashraturi33@gmail.com>
2021-06-20 03:48:32 +0530

    uploading modules Akash Raturi

---
+++ rahuljkm/Akash/modules/ecr/variables.tf
@@ -0,0 +1,17 @@
+variable "ecr_repo_name" {
+  description = "Provide ECR repo name"
+  type        = string
+  default     = "terraform_km_akash"
+}
+
+variable "scan_on_push" {
+  description = "Scan on push"
+  type        = bool
+  default     = true
+}
+
+variable "image_tag_mutability" {
+  description = "image_tag_mutability"
+  type        = string
+  default     = "MUTABLE"
+}

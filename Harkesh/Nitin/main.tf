5e89a1c026fb1b5ba22f5b010b8a23cd32096cf0
nitinpainoli <nitin.nn711@gmail.com>
2021-06-19 13:10:43 +0530

    adding updating main.tf

--- Harkesh/Nitin/main.tf
+++ Harkesh/Nitin/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "Nitin" {
-  name                 = "nitin"
+resource "aws_ecr_repository" "harkesh_nitin" {
+  name                 = "harkesh_nitin"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {

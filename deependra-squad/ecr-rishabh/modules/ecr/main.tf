10cacdcf4da5a87e092b3c356f5fed8213490326
Rishabh Gupta <rishabh.gupta1@tothenew.com>
2021-06-19 14:32:34 +0530

    upgrade terraform to new version

--- deependra-squad/ecr-rishabh/modules/ecr/main.tf
+++ deependra-squad/ecr-rishabh/modules/ecr/main.tf
@@ -1,7 +1,7 @@
 ### Resource for ECR
 resource "aws_ecr_repository" "rishabh" {
   name                 = var.ecr_repo_name 
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true

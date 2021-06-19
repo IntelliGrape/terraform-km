db9e1a93c0f0304efb93b1b928d7204a8f9db63c
nitinpainoli <nitin.nn711@gmail.com>
2021-06-19 12:24:03 +0530

    changes in main tf

--- Harkesh/Nitin/main.tf
+++ Harkesh/Nitin/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "myrepo"
+resource "aws_ecr_repository" "Nitin" {
+  name                 = "nitin"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {

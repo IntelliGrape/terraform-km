bd96adb6b0589ac3aca9289d6463e284d0afeb2a
theakshayraj <theakshayraj@outlook.com>
2021-06-19 13:48:26 +0530

    Akshay' New Commit

--- rahuljkm/Akshay/main.tf
+++ rahuljkm/Akshay/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "terraform-Akash-km"
+  name                 = "terraform-Akshay-km"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {

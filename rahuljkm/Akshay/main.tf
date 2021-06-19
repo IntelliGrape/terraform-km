61b1adc5361b32262b2936712d6316a7d6acf47d
theakshayraj <theakshayraj@outlook.com>
2021-06-19 13:45:10 +0530

    Akshay' 2nd Commit

--- rahuljkm/Akshay/main.tf
+++ rahuljkm/Akshay/main.tf
@@ -1,14 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = "terraform-Akash-km"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
   }
 }
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
--- rahuljkm/Akshay/provider.tf
+++ rahuljkm/Akshay/provider.tf
@@ -1,8 +1,14 @@
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "terraform-Akshay-km"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+

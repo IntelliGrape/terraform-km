38c20e94a9e71cd3de11e0c361af9ea21036a98f
Sanyam Munjal <sanyam.munjal@tothenew.com>
2021-06-19 14:27:05 +0530

    cleanup1

--- sanyam/somesh/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "somesh-devops-km"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-}

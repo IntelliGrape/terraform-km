c372b8cb8811912508e7afb960f9318c19ab8cf9
Nihit Jain <nihit.jain@tothenew.com>
2021-06-19 12:59:17 +0530

    tf changes

--- Harkesh/Nihit/main.tf
+++ Harkesh/Nihit/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "foo" {
-  name                 = "devops-session"
+  name                 = "terraform-km-nihit"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
